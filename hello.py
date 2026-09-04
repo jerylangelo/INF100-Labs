2248882375bce35b92d9578d12d8c76b1ffc6feb
jerylangelo <najeryl@gmail.com>
2026-09-04 10:13:11 +0800

    second post

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ print("=========================")
 print("Welcome here")
 print("my first post!")
 print("=========================")
+
+
+username = "jerylangelo"
+bio = "fun troller"
+followers = 67
+
+print("Username", username)
+print("Bio:", bio)
+print("Followers:", followers)
