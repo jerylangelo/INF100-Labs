02103f892efb7d4b0ad97eeb4cddbfa1ab6ed680
jerylangelo <najeryl@gmail.com>
2026-09-04 10:16:16 +0800

    third post

--- hello.py
+++ hello.py
@@ -8,6 +8,17 @@ username = "jerylangelo"
 bio = "fun troller"
 followers = 67
 
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2",followers)
+
+followers -= 10 
+print("Day 3", followers)
+
+
 print("Username", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
